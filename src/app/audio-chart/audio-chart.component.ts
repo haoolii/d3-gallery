a026416f9f035ab35bb8a1f7661abdb18ed5425b
Hao <Hao@miniasp.com>
2021-01-18 17:02:13 +0800

    feat: 修改Audio

--- src/app/audio-chart/audio-chart.component.ts
+++ src/app/audio-chart/audio-chart.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { constants } from 'buffer';
 import * as d3 from 'd3';
+import { from, Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-audio-chart',
@@ -31,83 +32,118 @@ export class AudioChartComponent implements OnInit {
 
   beatData: AudioBuffer;
 
-  sampleCount = 8;
+  waveData: Float32Array;
 
   playing = false;
+
+  xScaleWaveData: d3.ScaleLinear<number, number>;
+
+  xScaleDuration: d3.ScaleLinear<number, number>;
+
+  line: d3.Line<any>;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.posLayer
-          .append('line')
-          .attr("y2", this.height)
-          .attr('stroke-width', 2)
-          .attr('stroke', 'red')
-      this.getAudio('assets/music.mp3')
-          .subscribe(buffer => this.audioCtx.decodeAudioData(buffer, buffer => this.loadAudio(buffer)))
+    Promise.resolve().then(() => {
+      this.goJustFirst();
+      this.go();
+    })
+  }
+
+  goJustFirst(): void {
+    this.posLayer
+        .append('line')
+        .attr("y2", this.height)
+        .attr('stroke-width', 2)
+        .attr('stroke', 'red')
+  }
+
+  go(): void {
+    this.initAudio()
+        .subscribe(buffer => {
+          this.beatData = buffer;
+          this.waveData = buffer.getChannelData(0).filter((d, i) => i % 10);
+          this.compute();
+          this.render();
     });
   }
 
-  getAudio(url: string) {
-    return this.http.get(url, { responseType: 'arraybuffer' });
+  compute(): void {
+    this.computeScale();
+    this.computeLine();
   }
 
-  loadAudio(buffer: AudioBuffer) {
-    this.beatData = buffer;
-    console.log(this.beatData);
-    let waveData = buffer.getChannelData(0).filter((d, i) => i % 10);
-    console.log('waveData', waveData)
-    let xScale = d3.scaleLinear().range([0, this.width]).domain([0, waveData.length]);
-    let line = d3.line<any>()
-                .x((d, i) => xScale(i))
-                .y(d => this.waveHeight / 2 * d + this.waveHeight/2)
+  computeScale(): void {
+    this.xScaleWaveData = d3.scaleLinear()
+        .domain([0, this.waveData.length])
+        .range([0, this.width]);
 
+    this.xScaleDuration = d3.scaleLinear()
+        .domain([0, this.beatData.duration])
+        .range([0, this.width]);
+  }
+
+  computeLine(): void {
+    this.line = d3.line<any>()
+        .x((d, i) => this.xScaleWaveData(i))
+        .y(d => this.waveHeight / 2 * d + this.waveHeight/2)
+  }
+
+  render(): void {
+      this.renderWaveData();
+  }
+
+  renderWaveData(): void {
     this.audioLayer
         .append('path')
-        .datum(waveData)
-        .attr('d', line);
+        .datum(this.waveData)
+        .attr('d', this.line);
   }
+
+  renderLine(): void {
+    this.posLayer.select('line')
+  }
+
+  isPlay = false;
+  startTime = 0;
 
   playSound(i: number): void {
     if (this.source) {
       this.source.stop();
     }
     this.source = this.audioCtx.createBufferSource();
-    this.source.buffer = this.beatData;
     this.source.connect(this.audioCtx.destination);
-    this.source.start(0);
-    let line = this.posLayer.select('line');
-    let b = this.beatData;
-    let xS = d3.scaleLinear().range([0, this.width]).domain([0, b.duration]);
-    let au = this.audioCtx;
-    let st = au.currentTime;
-    positionSlide()
-    function positionSlide() {
-      console.log('d', au.currentTime - st);
-
-      line
-          .transition().duration(b.duration / 100)
-          .attr("transform", "translate(" + xS(au.currentTime - st) + ")")
-          .on("end", d => {
-            if (b.duration > (au.currentTime - st) )
-            positionSlide()
-          });
-    }
-
-    // if (this.playing) {
-    //   this.playing = false;
-    //   this.source.disconnect(this.audioCtx.destination);
-    //   return;
-    // } else {
-    //   this.playing = true;
-    //   console.log('console.log(this.audioCtx.currentTime);', this.audioCtx.currentTime);
-    //   this.source.connect(this.audioCtx.destination);
-    //   return;
-    // }
+    this.source.buffer = this.beatData;
+    this.source.start();
+    this.startTime = this.audioCtx.currentTime;
+    this.positionSlide();
   }
 
-    /**
+  positionSlide(): void {
+
+    this.posLayer.select('line')
+        .transition()
+        .duration(this.beatData.duration / 10)
+        .attr('transform', `translate(${this.xScaleDuration(this.audioCtx.currentTime - this.startTime)})`)
+        .on("end", d => {
+          if ((this.audioCtx.currentTime - this.startTime) < this.beatData.duration) {
+            this.positionSlide();
+            this.renderLine();
+          }
+        });
+  }
+
+  initAudio(): Observable<AudioBuffer> {
+    return this.getAudio('assets/music.mp3')
+              .pipe(mergeMap(buffer => from(this.audioCtx.decodeAudioData(buffer))));
+  }
+
+  getAudio(url: string) {
+    return this.http.get(url, { responseType: 'arraybuffer' });
+  }
+
+  /**
    * 取得個圖層
    */
   get svgSelection(): d3.Selection<any, unknown, null, undefined> {
